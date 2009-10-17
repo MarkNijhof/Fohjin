60da4b8c151bb2b3267df42f75b87f02da78ab22
Mark Nijhof <Mark.Nijhof@Gmail.com>
2009-10-17 23:58:10 +0200

    Fixed a typo Changed a test namespace

---
+++ Fohjin.DDD/Fohjin.DDD.BankApplication/Views/ClientSearchForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Fohjin.DDD.BankApplication.Views
+{
+    partial class ClientSearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.clientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addANewClientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshExistingClientsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this._clients = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clientToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(306, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // clientToolStripMenuItem
+            // 
+            this.clientToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addANewClientToolStripMenuItem,
+            this.refreshExistingClientsToolStripMenuItem});
+            this.clientToolStripMenuItem.Name = "clientToolStripMenuItem";
+            this.clientToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.clientToolStripMenuItem.Text = "Client";
+            // 
+            // addANewClientToolStripMenuItem
+            // 
+            this.addANewClientToolStripMenuItem.Name = "addANewClientToolStripMenuItem";
+            this.addANewClientToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.addANewClientToolStripMenuItem.Text = "Add a new client";
+            this.addANewClientToolStripMenuItem.Click += new System.EventHandler(this.addANewClientToolStripMenuItem_Click);
+            // 
+            // refreshExistingClientsToolStripMenuItem
+            // 
+            this.refreshExistingClientsToolStripMenuItem.Name = "refreshExistingClientsToolStripMenuItem";
+            this.refreshExistingClientsToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.refreshExistingClientsToolStripMenuItem.Text = "Refresh existing clients";
+            this.refreshExistingClientsToolStripMenuItem.Click += new System.EventHandler(this.refreshExistingClientsToolStripMenuItem_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(306, 158);
+            this.tabControl1.TabIndex = 22;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(298, 132);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this._clients);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(285, 119);
+            this.groupBox1.TabIndex = 22;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Existing clients";
+            // 
+            // _clients
+            // 
+            this._clients.FormattingEnabled = true;
+            this._clients.Location = new System.Drawing.Point(6, 19);
+            this._clients.Name = "_clients";
+            this._clients.Size = new System.Drawing.Size(273, 95);
+            this._clients.TabIndex = 7;
+            this._clients.DoubleClick += new System.EventHandler(this._clients_DoubleClick);
+            // 
+            // ClientSearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 182);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ClientSearchForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Client Search Form";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem clientToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addANewClientToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshExistingClientsToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox _clients;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
---
+++ Fohjin.DDD/Fohjin.DDD.BankApplication/Views/ClientSearchForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Fohjin.DDD.BankApplication.Presenters;
+using Fohjin.DDD.Reporting.Dto;
+
+namespace Fohjin.DDD.BankApplication.Views
+{
+    public partial class ClientSearchForm : Form, IClientSearchFormView
+    {
+        private IClientSearchFormPresenter _presenter;
+
+        public ClientSearchForm()
+        {
+            InitializeComponent();
+            tabControl1.Appearance = TabAppearance.FlatButtons;
+            tabControl1.ItemSize = new Size(0, 1);
+            tabControl1.SizeMode = TabSizeMode.Fixed;
+        }
+
+        public IEnumerable<Client> Clients
+        {
+            get { return (IEnumerable<Client>)_clients.DataSource; }
+            set { _clients.DataSource = value; }
+        }
+
+        public void SetPresenter(IClientSearchFormPresenter clientSearchFormPresenter)
+        {
+            _presenter = clientSearchFormPresenter;
+        }
+
+        public Client GetSelectedClient()
+        {
+            return (Client)_clients.SelectedItem;
+        }
+
+        private void _clients_DoubleClick(object sender, EventArgs e)
+        {
+            _presenter.OpenSelectedClient();
+        }
+
+        private void addANewClientToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _presenter.CreateNewClient();
+        }
+
+        private void refreshExistingClientsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _presenter.Refresh();
+        }
+    }
+}
